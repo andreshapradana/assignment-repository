3b085fea50336a8aa6b15d8f2a5417a1942375dd
andreshapradana <andresha.pradana@gmail.com>
2022-09-13 13:34:50 +0700

    Yes

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+def show_catalog(request):
+    data_catalog = CatalogItem.objects.all()
+    context = {
+        'list_barang': data_catalog,
+        'nama': 'Andresha Pradana', 
+        'NPM' : '2106651591'
+    }
+    return render(request, "katalog.html", context)
